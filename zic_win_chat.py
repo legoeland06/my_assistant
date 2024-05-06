61b7786a030318a121e8679b0fd32cac2fd3a7dd
legoeland06 <45659378+legoeland06@users.noreply.github.com>
2024-05-06 17:33:58 +0200

    add functionalities, like, save_to_mp3 and reading pdfs(in work)

--- zic_win_chat.py
+++ zic_win_chat.py
@@ -4,9 +4,11 @@ import datetime
 import threading
 import tkinter as tk
 from tkinter import StringVar, messagebox
+from tkinter import filedialog
+from tkinter import simpledialog
+import PyPDF2
 from tkhtmlview import HTMLLabel
 from PIL import Image, ImageTk
-import os
 import markdown.util
 import vosk
 import pyaudio
@@ -16,6 +18,8 @@ import ollama
 import markdown
 import imageio.v3 as iio
 import subprocess
+from spacy.lang.fr import French
+from spacy.lang.en import English
 
 from PIL import Image, ImageTk
 
@@ -125,6 +129,10 @@ LIENS_CHROME = {
     "message": "https://messages.google.com/web/conversations/151",
 }
 
+BANNIERE_WIDTH = 758
+BANNIERE_HEIGHT = 160
+FENETRE_WIDTH = 800
+FENETRE_HEIGHT = 400
 
 PROMPTS_SYSTEMIQUES = {
     SPECIALITY: "Bonjour ! Je souhaite me former à [ speciality ], devenir un top expert sur le sujet. Peux-tu me proposer un programme de formation avec les thématiques à étudier, dans un ordre pertinent ? Tu es un expert en [ speciality ] et aussi un formateur confirmé. Base toi sur tes connaissances en [ speciality ] mais aussi en science de l'éducation pour me proposer le meilleur programme possible. Après ça, je te demanderai de me former sur chacun des points de ton programme",
@@ -198,6 +206,18 @@ def engine_lecteur_init():
 
     pyttsx3.speak("lancement...")
     return lecteur
+
+def read_pdf(book, pg_no):
+    pdf_Reader = PyPDF2.PdfFileReader(book)
+    pages = pdf_Reader.numPages
+
+    speaker = pyttsx3.init()
+
+    for num in range((pg_no - 1), pages):
+        page = pdf_Reader.getPage(num)
+        text = page.extractText()
+        speaker.say(text)
+        speaker.runAndWait()
 
 
 def lancer_chrome(url: str) -> subprocess.Popen[str]:
@@ -494,7 +514,9 @@ class Fenetre_entree:
         self, msg_to_write, moteur_de_diction, title="Lecteur|traducteur"
     ):
         def appel_dicter():
-            image = ImageTk.PhotoImage(Image.open("banniere.jpeg").resize((800, 160)))
+            image = ImageTk.PhotoImage(
+                Image.open("banniere.jpeg").resize((BANNIERE_WIDTH, BANNIERE_HEIGHT))
+            )
             dicter(image=image)
 
         def dicter(image):
@@ -504,6 +526,7 @@ class Fenetre_entree:
                 image=image,
                 fenetre=fenetre_dictee,
                 message="... Jonathan Livingston dit legoeland",
+                quitter=quitter,
             )
 
             def transferer_contenu():
@@ -608,7 +631,7 @@ class Fenetre_entree:
         def quitter() -> str:
             # Afficher une boîte de message de confirmation
             if messagebox.askyesno(
-                "Confirmation", "Êtes-vous sûr de vouloir soumetre ?"
+                "Confirmation", "Êtes-vous sûr de vouloir quitter ?"
             ):
                 save_to_submission()
                 fenetre.destroy()
@@ -626,6 +649,35 @@ class Fenetre_entree:
                 finally:
                     moteur_de_diction(texte_to_talk)
 
+        def clear_entree1():
+            entree1.replace("1.0", tk.END, "")
+
+        def clear_entree2():
+            entree2.set_html("")
+
+        def text_to_mp3(object: tk.Text):
+            texte_to_save_to_mp3 = object.get("1.0", tk.END)
+
+            if texte_to_save_to_mp3 != "":
+                try:
+                    texte_to_save_to_mp3 = object.selection_get()
+                except:
+                    texte_to_save_to_mp3 = object.get("1.0", tk.END)
+                finally:
+                    moteur_de_diction("transcription du texte vers un fichier mp3")
+                    simple_dialog = simpledialog.askstring(
+                        parent=fenetre,
+                        prompt="Enregistrement : veuillez choisir un nom au fichier",
+                        title="Enregistrer vers audio",
+                    )
+                    lecteur.save_to_file(texte_to_save_to_mp3, simple_dialog.lower() + ".mp3")
+                    moteur_de_diction("terminé")
+            else : 
+                moteur_de_diction("Désolé, Il n'y a pas de texte à enregistrer en mp3")
+
+        def entree1_to_mp3():
+            text_to_mp3(entree1)
+
         def lire_texte1():
             lire_text_from_object(entree1)
 
@@ -638,150 +690,179 @@ class Fenetre_entree:
             if ponctuel:
                 html_entries += (
                     "<strong style='color:grey;'>Question:</strong>"
-                    + '<span style="font-size: 12px;color:grey;text-align:justify">'
+                    + '<span style="font-size: 12;color:grey;text-align:justify">'
                     + self.get_submission()
                     + "</span>"
                     + "<strong style='color:red;'>Réponse:</strong>"
-                    + '<span style="font-size: 12px;color:brown;text-align:justify">'
+                    + '<span style="font-size: 12;color:brown;text-align:justify">'
                     + markdown_content
                     + "</span>"
                     + "</span>"
                 )
-            else :
-                html_entries += (
-                    '<span style="font-size: 12px;color:brown;text-align:justify">'
-                    + markdown_content
-                )
+            else:
+                html_entries += markdown_content
 
             entree2.set_html(html_entries)
-            entree2.fit_height()
             entree2.update()
-            entree2.bind()
 
-        def refresh_entree1_html(texte: str, index1: str, index2: str,ponctuel:bool=True):
+        def replace_in_place(
+            texte: str, index1: str, index2: str, ponctuel: bool = True
+        ):
             entree1.replace(chars=texte, index1=index1, index2=index2)
-            # entree1.insert(index=index, chars=texte)
-            entree1.update()
 
         def translate_inplace():
+            was_a_list = False
             try:
                 texte_initial = entree1.selection_get()
                 indx1 = entree1.index(tk.SEL_FIRST)
                 indx2 = entree1.index(tk.SEL_LAST)
-                
+
                 texte_traite = traitement_du_texte(texte_initial, 500)
                 if isinstance(texte_traite, list):
                     for element in texte_traite:
                         translated_text = str(translate_it(text_to_translate=element))
-                        refresh_entree1_html(texte=translated_text, index1=indx1, index2=indx2,ponctuel=False)
+                        replace_in_place(
+                            texte=translated_text,
+                            index1=indx1,
+                            index2=indx2,
+                            ponctuel=False,
+                        )
                 else:
                     translated_text = str(translate_it(text_to_translate=texte_traite))
-                    refresh_entree1_html(texte=translated_text, index1=indx1, index2=indx2,ponctuel=True)
+                    replace_in_place(
+                        texte=translated_text, index1=indx1, index2=indx2, ponctuel=True
+                    )
 
-                entree1.pack(fill="both", expand=True)
             except:
                 texte_initial = entree1.get("1.0", tk.END)
                 texte_traite = traitement_du_texte(texte_initial, 500)
                 if isinstance(texte_traite, list):
                     for element in texte_traite:
                         translated_text = str(translate_it(text_to_translate=element))
-                        refresh_entree_html(translated_text,False)
+                        refresh_entree_html(translated_text, False)
+                    was_a_list = True
+                elif was_a_list == True:
+                    translated_text = str(translate_it(text_to_translate=texte_traite))
+                    refresh_entree_html(translated_text, False)
                 else:
                     translated_text = str(translate_it(text_to_translate=texte_traite))
-                    refresh_entree_html(translated_text,True)
-
-                entree2.pack(fill="both", expand=True)
-
-                # Création d'un bouton pour Lire
-                bouton_lire2.pack(side=tk.RIGHT)
-                canvas2.pack(fill="both", expand=True)
-
-        def translate_this_text():
-            try:
-                texte_initial = entree1.selection_get()
-            except:
-                texte_initial = entree1.get("1.0", tk.END)
-            finally:
-                texte_traite = traitement_du_texte(texte_initial, 500)
-                if isinstance(texte_traite, list):
-                    for element in texte_traite:
-                        translated_text = str(translate_it(text_to_translate=element))
-                        refresh_entree_html(translated_text,False)
-                else:
-                    translated_text = str(translate_it(text_to_translate=texte_traite))
-                    refresh_entree_html(translated_text,True)
-
-                entree2.pack(fill="both", expand=True)
-                # Création d'un bouton pour Lire
-                bouton_lire2.pack(side=tk.RIGHT)
-                canvas2.pack(fill="both", expand=True)
+                    refresh_entree_html(translated_text, True)
 
         # Création de la fenêtre principale
         fenetre = tk.Tk()
         fenetre.title(self.title + " - " + title)
-        fenetre.configure(width=800)
+        fenetre.configure(width=str(FENETRE_WIDTH), height=str(FENETRE_HEIGHT))
 
-        my_image = ImageTk.PhotoImage(Image.open("banniere.jpeg").resize((800, 160)))
-        affiche_illustration(my_image, fenetre, "... Jonathan Livingston dit legoeland")
+        my_image = ImageTk.PhotoImage(
+            Image.open("banniere.jpeg").resize((BANNIERE_WIDTH, BANNIERE_HEIGHT))
+        )
+        affiche_illustration(
+            my_image, fenetre, "... Jonathan Livingston dit legoeland", quitter=quitter
+        )
 
-        # Création des boutons
         button_frame = tk.Frame(fenetre, relief="sunken")
-        button_frame.pack(fill="x", expand=False)
-        # donne moi une liste de 10 questions à propos de l'utilisation de la biliothèque asyncio en python
-        canvas1 = tk.Frame(fenetre, relief="sunken")
-        canvas1.pack(fill="x", expand=False)
+        button_frame.pack(fill="x", expand=True)
+        canvas_edition = tk.Frame(fenetre, relief="sunken")
 
-        # Création d'un champ de saisie de l'utilisateur
+        canvas1 = tk.Frame(canvas_edition, relief="sunken")
+        boutons_effacer_canvas2 = tk.Frame(canvas_edition)
+        canvas2 = tk.Frame(canvas_edition, relief="sunken")
+
+        canvas_edition.pack(fill="x", expand=True)
+        canvas1.pack(fill="x", expand=True)
+        boutons_effacer_canvas2.pack(fill="x", expand=True)
+        canvas2.pack(fill="x", expand=True)
+
         entree1 = tk.Text(canvas1)
-        entree1.configure(bg="grey", fg="white", font=("arial", 14))
+        # Attention la taille de la police, ici 10, ce parametre tant à changer le cadre d'ouverture de la fenetre
+        entree1.configure(bg="grey", fg="white", font=("arial", 10))
 
+        boutton_effacer_entree1 = tk.Button(
+            button_frame, text="X", command=clear_entree1
+        )
+        boutton_effacer_entree1.configure(bg="red", fg="white")
+        boutton_effacer_entree1.pack(side="right")
         entree1.insert(tk.END, msg_to_write)
         entree1.focus_set()
-        entree1.pack(fill="x", expand=False)
-
-        canvas2 = tk.Frame(fenetre, relief="sunken")
+        entree1.pack(fill="both", expand=True)
 
         # Création d'un champ de saisie de l'utilisateur
-        entree2 = HTMLLabel(canvas2)
-        entree2.configure(bg="white", fg="brown",font=("arial ", 12))
+        boutton_effacer_entree2 = tk.Button(
+            boutons_effacer_canvas2, text="X", command=clear_entree2
+        )
 
-        entree2.pack(fill="x", expand=False)
-        canvas2.pack(fill="x", expand=False)
+        boutton_effacer_entree2.configure(bg="red", fg="white")
+        boutton_effacer_entree2.pack(side="right")
+        bouton_lire2 = tk.Button(
+            boutons_effacer_canvas2, text="Lire", command=lire_texte2
+        )
+        bouton_lire2.configure(bg="green", fg="white")
+        bouton_lire2.pack(side=tk.RIGHT)
+        entree2 = HTMLLabel(canvas2)
+        entree2.configure(bg="white", fg="brown", font=("arial ", 12))
+        entree2.pack(fill="both", expand=True)
 
         # Création d'un bouton pour Lire
-        bouton_lire1 = tk.Button(button_frame, text="Lire", command=lire_texte1)
+        bouton_lire1 = tk.Button(
+            button_frame, text="Lire la sélection", command=lire_texte1
+        )
+        bouton_lire1.configure(
+            bg=_from_rgb((0, 0, 0)),
+            fg="white",
+            highlightbackground="red",
+            highlightcolor="white",
+        )
         bouton_lire1.pack(side=tk.LEFT)
-        bouton_lire1.configure(bg="grey", fg="white")
-
-        bouton_lire2 = tk.Button(button_frame, text="Lire", command=lire_texte2)
-        bouton_lire2.configure(bg="green", fg="white")
-
-        # Création d'un bouton pour Dicter
-        bouton_dicter = tk.Button(
-            button_frame, text="Mode de diction", command=appel_dicter
-        )
-        bouton_dicter.configure(bg="black", fg="white")
-        bouton_dicter.pack(side=tk.LEFT)
-
-        # Création d'un bouton pour traduction
-        bouton_traduire = tk.Button(
-            button_frame, text="Traduire", command=translate_this_text
-        )
-        bouton_traduire.pack(side=tk.LEFT)
 
         # Création d'un bouton pour traduction_sur_place
         bouton_traduire_sur_place = tk.Button(
             button_frame, text="...Traduire en place...", command=translate_inplace
         )
+        bouton_traduire_sur_place.configure(
+            bg=_from_rgb((40, 40, 40)),
+            fg="white",
+            highlightbackground="red",
+            highlightcolor="white",
+        )
         bouton_traduire_sur_place.pack(side=tk.LEFT)
 
+        # Création d'un bouton pour Dicter
+        bouton_dicter = tk.Button(
+            button_frame,
+            text="Passer en Mode diction",
+            command=appel_dicter,
+            highlightbackground="red",
+            highlightcolor="white",
+        )
+        bouton_dicter.configure(
+            bg=_from_rgb((80, 80, 80)),
+            fg="white",
+        )
+        bouton_dicter.pack(side=tk.LEFT)
+
         # Création d'un bouton pour soumetre
-        bouton_soumetre = tk.Button(button_frame, text="Soumettre", command=soumettre)
+        bouton_soumetre = tk.Button(
+            button_frame, text="Soumettre à l'IA", command=soumettre
+        )
+        bouton_soumetre.configure(
+            bg=_from_rgb((120, 120, 120)),
+            fg="white",
+            highlightbackground="red",
+            highlightcolor="white",
+        )
         bouton_soumetre.pack(side=tk.LEFT)
 
-        # Création d'un bouton pour quitter
-        bouton_quitter = tk.Button(button_frame, text="Quitter", command=quitter)
-        bouton_quitter.pack(side=tk.RIGHT)
+        bouton_save_to_mp3 = tk.Button(
+            button_frame, text="texte vers mp3", command=entree1_to_mp3
+        )
+        bouton_save_to_mp3.configure(bg=_from_rgb((160, 160, 160)), fg="black")
+        bouton_save_to_mp3.pack(side="left")
+
+        # NE fonctionne pas pour mettre en pause la lecture à haute voix
+        # bouton_stop=tk.Button(button_frame,text="Stop",command=lecteur.endLoop)
+        # bouton_reprendre=tk.Button(button_frame,text="reprendre",command=lecteur.startLoop)
+        # bouton_stop.pack(side=tk.LEFT)
+        # bouton_reprendre.pack(side=tk.LEFT)
 
         fenetre.mainloop()
 
@@ -798,6 +879,15 @@ def traitement_du_texte(texte: str, number: int) -> list[list[str]]:
         on envois le texte telquel
     ### RETURN : str ou List
     """
+
+    # my_nlp=French()
+    # my_doc_texte=my_nlp.make_doc(texte)
+    # my_liste_doc=my_doc_texte.cats
+    # my_doc_texte.retokenize()
+    # span = my_liste_doc[1:3]
+    # # Get the span text via the .text attribute
+    # print(span.text)
+
     # on découpe le texte par mots
     liste_of_words = texte.split()
     if len(liste_of_words) >= number:
@@ -816,7 +906,7 @@ def traitement_du_texte(texte: str, number: int) -> list[list[str]]:
         return texte
 
 
-def affiche_illustration(image, fenetre, message):
+def affiche_illustration(image, fenetre, message, quitter):
     """affiche l'illustration du goeland ainsi que son slogan"""
     # ## PRESENTATION DU GOELAND  ####
     cnvs1 = tk.Frame(fenetre)
@@ -824,18 +914,22 @@ def affiche_illustration(image, fenetre, message):
     cnvs1.pack(fill="x", expand=False)
     # ################################
     cnvs2 = tk.Frame(cnvs1)
-    cnvs2.configure(bg="grey")
+    cnvs2.configure(bg="black")
     cnvs2.pack(fill="x", expand=False)
 
     # Create a canvas
-    canva = tk.Canvas(cnvs1, height=160, width=800)
+    canva = tk.Canvas(cnvs1, height=BANNIERE_HEIGHT, width=BANNIERE_WIDTH)
 
+    # Création d'un bouton pour quitter
+    bouton_quitter = tk.Button(cnvs2, text="Quitter", command=quitter)
+    bouton_quitter.configure(bg="black", fg="red")
+    bouton_quitter.pack(side=tk.LEFT)
     label = tk.Label(
         cnvs2,
         text=message,
         font=("Trebuchet", 8),
         fg="white",
-        bg="grey",
+        bg="black",
     )
     label.pack(side=tk.RIGHT, expand=False)
 
@@ -853,6 +947,10 @@ def _from_rgb(rgb):
 lecteur = engine_lecteur_init()
 
 
+def save_to_mp3(object: tk.Text):
+    pass
+
+
 def main(prompt=False, stop_talking=False):
 
     def inc_lecteur():
@@ -867,19 +965,21 @@ def main(prompt=False, stop_talking=False):
 
     async def dire_tt(alire: str):
         lecteur.say(alire)
+        
         lecteur.runAndWait()
+
+        # lecteur.endLoop()
         return lecteur.stop()
 
     # TODO : loop async for saytt
-    def start_loop_saying(texte:str):
+    def start_loop_saying(texte: str):
         loop = asyncio.new_event_loop()
         asyncio.set_event_loop(loop)
         loop.run_until_complete(loop.create_task(dire_tt(alire=texte)))
 
-    def say_tt(alire:str):
-        threading.Thread(target=start_loop_saying(texte=alire)).start()
-
-
+    def say_tt(alire: str):
+        the_thread=threading.Thread(target=start_loop_saying(texte=alire))
+        the_thread.start()
 
     def say_txt(alire: str, stop_ecoute: bool):
         if not stop_talking:
@@ -903,9 +1003,6 @@ def main(prompt=False, stop_talking=False):
 
     say_txt("IA initialisée ! ", stop_ecoute=False)
 
-    def text_to_mp3(content: str, lecteur):
-        lecteur.save_to_file(content, "save.mp3")
-
     def arret_ecoute():
         stream.stop_stream()
 
@@ -923,7 +1020,11 @@ def main(prompt=False, stop_talking=False):
         + "\nChargement... Veuillez patienter\n"
         + STARS * WIDTH_TERM
     )
+
+    # prend beaucoup de temp
+    # passer ça en asynchrone
     model_ecouteur_micro = engine_ecouteur_init()
+
     say_txt("micro audio initialisé", False)
 
     # Create a recognizer
@@ -939,7 +1040,7 @@ def main(prompt=False, stop_talking=False):
         input=True,
         frames_per_buffer=8192,
     )
-
+    # Fenetre_entree(any,any,any,any)
     Fenetre_entree(
         stream=stream,
         lecteur=say_tt,
